73bc7122ca52328f45c68646a1f0c29654637bae
jlao3 <jlao3@ucsc.edu>
2020-06-08 11:48:52 -0700

    Bullets

--- FinalProject/Assets/PBR Weapons Pack/AK74M/GunController.cs
+++ FinalProject/Assets/PBR Weapons Pack/AK74M/GunController.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class GunController : MonoBehaviour
 {
     Animator m_animator;
+    
+    public GameObject mainBullet;
+    public float speed = 100f;
 
     void Start()
     {
@@ -17,6 +22,19 @@ public class GunController : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             m_animator.SetTrigger("Shoot");
+            Bullet();
         }
+    }
+
+    void Bullet()
+    {
+        float angle = 90;
+        float rotation = 90;
+        GameObject instBullet = Instantiate(mainBullet, transform.position, Quaternion.identity) as GameObject;
+        instBullet.transform.position = new Vector3(3.2f, 1.564f, -0.7952048f);
+        instBullet.transform.rotation = Quaternion.Euler(angle, 0 , rotation);
+        Rigidbody instBulletRigidbody = instBullet.GetComponent<Rigidbody>();
+        instBulletRigidbody.AddForce(Vector3.left * speed);
+
     }
 }
